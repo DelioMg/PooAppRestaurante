9bf7b3f8b57d5818059028d44ddb3ba1498a3c62
Délio <delio_mg@hotmail.com>
2022-04-04 16:28:01 -0300

    Update FotoProduto.java

--- Projeto/src/main/java/produto/FotoProduto.java
+++ Projeto/src/main/java/produto/FotoProduto.java
@@ -9,12 +9,65 @@ package produto;
  * @author PC
  */
 public class FotoProduto {
-    	private String nome;
+    
+	private String nome;
+        private String descricao;
+        private String contentType;
+ 	private long tamanho;
 
-	private String descricao;
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
 
-	private String contentType;
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	private long tamanho;
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
 
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the contentType
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * @param contentType the contentType to set
+     */
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    /**
+     * @return the tamanho
+     */
+    public long getTamanho() {
+        return tamanho;
+    }
+
+    /**
+     * @param tamanho the tamanho to set
+     */
+    public void setTamanho(long tamanho) {
+        this.tamanho = tamanho;
+    }
 }
